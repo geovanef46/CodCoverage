a8496ea83d05f627d07f7c198c3ed4a4f38f3884
jason <geovanef46@gmail.com>
2019-09-21 19:42:59 -0300

    update

--- src/codcoverage/CodCoverage.java
+++ src/codcoverage/CodCoverage.java
@@ -6,7 +6,12 @@
 package codcoverage;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import org.apache.commons.io.FileUtils;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -30,6 +35,7 @@ public String insertFile() {
         String source="";
         try {
          source = FileUtils.readFileToString(new File("src/codcoverage/source/src/example/Triangle.java"), "UTF-8");
+         System.out.println("Arquivo obtido!");
         } catch (Exception e) {
             System.out.println("Deu ruim... não encontrou o arquivo");
         }
@@ -39,17 +45,18 @@ public String insertFile() {
 
  
  public boolean insertHello() { 
-     
+     Logger log = logIn("loggin");
      ASTParser parser = ASTParser.newParser(AST.JLS8);
      parser.setSource(insertFile().toCharArray());
      parser.setKind(ASTParser.K_COMPILATION_UNIT);
 
- CompilationUnit unit = (CompilationUnit) parser.createAST(new NullProgressMonitor());
+ CompilationUnit unit = (CompilationUnit) parser.createAST(null);
  AST ast = unit.getAST();
 
  List<MethodDeclaration> methodDeclarations = MethodDeclarationFinder.perform(unit);
  for (MethodDeclaration methodDeclaration : methodDeclarations) {
-     MethodInvocation methodInvocation = ast.newMethodInvocation();
+     log.log(Level.INFO, methodDeclaration.getName().getFullyQualifiedName());
+//     MethodInvocation methodInvocation = ast.newMethodInvocation();
 //
 //     // System.out.println("Hello, World")
 //     QualifiedName qName = ast.newQualifiedName(ast.newSimpleName("System"), ast.newSimpleName("out"));
@@ -66,6 +73,23 @@ public String insertFile() {
  return true;
  }
  
+ 
+ public Logger logIn(String name) {
+     
+     Logger log = Logger.getLogger("Log");
+     FileHandler filetxt = null;
+     try {
+         filetxt = new FileHandler(name+".txt");
+         filetxt.setFormatter(new SimpleFormatter());
+     } catch (SecurityException | IOException e1) {
+       
+         e1.printStackTrace();
+     }
+     log.addHandler(filetxt);
+     
+     return log;
+    
+}
  
  
  public static void main(String[] args) {
